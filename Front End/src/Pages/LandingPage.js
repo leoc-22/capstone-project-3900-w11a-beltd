22107d80b06230ab98f006a15ff1e02e7fe43fec
Bob Ma <bob.ma000@gmail.com>
2022-03-09 11:41:07 +1100

    front end

---
+++ Front End/src/Pages/LandingPage.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import LandingPageTopBar from '../Components/LandingPageTopBar';
+import Header from '../Components/Header'
+import CollectionsCarousel from '../Components/CollectionsCarousel'
+import TopBookGrid from '../Components/TopBookGrid'
+
+import { useHistory } from 'react-router-dom';
+import { makeStyles, List, ListItemIcon, ListItemText,   Grid, ListItem, IconButton} from '@material-ui/core';
+
+
+
+const useStyles = makeStyles({
+    main:{
+        minHeight: "1100px",
+        minWidth : "500px",
+
+    },
+    popularCollections : {
+        marginLeft : "7%",
+        marginTop: "5%"
+    },
+    TopBooks : {
+        marginLeft : "7%",
+        marginTop: "5%"
+
+    },
+    test: {
+    }
+})
+
+
+export default function LandingPage() {
+    const history = useHistory();
+    const classes = useStyles();
+
+    return (
+        <div>
+            <LandingPageTopBar>  
+            </LandingPageTopBar>
+
+            <div className={classes.main}>
+                
+                <Header ></Header>
+                    <h2 className={classes.popularCollections}>Popular Collections</h2>
+                
+                <CollectionsCarousel>
+                </CollectionsCarousel>
+                    <h2 className={classes.TopBooks}>Top Books</h2>
+                <TopBookGrid>
+                </TopBookGrid>
+
+            </div>
+        </div>
+    );
+}
