2ba22611a98dcf80caf8e218967e2b8e7dbaeaae
Leo Cao <z5261846@ad.unsw.edu.au>
2022-03-13 21:20:24 +1100

    sendmail progress

--- identity_service/src/tokenRouter.js
+++ identity_service/src/tokenRouter.js
@@ -1,5 +1,3 @@
-///
-
 const express = require("express");
 const app = express();
 
@@ -18,82 +16,74 @@ const bcrypt = require("bcrypt");
 const token = null;
 
 app.post("/forget", async (req, res) => {
-    var query = { email: req.body.email };
-    User.countDocuments(
-        { email: req.body.email },
-        async function (err, count) {
-            if (count > 0) {
-                console.log("found email");
-                //await requestPasswordReset(query);
-            } else {
-                console.log("user not found");
-            }
-        }
-    );
+  var query = { email: req.body.email };
+  User.countDocuments({ email: req.body.email }, async function (err, count) {
+    if (count > 0) {
+      console.log("found email");
+      await requestPasswordReset(query);
+    } else {
+      console.log("user not found");
+    }
+  });
 });
 
 app.put("/passwordReset?token:resetToken", async (req, res) => {
-    if (resetToken == token) {
-        updatePassword(email);
-    };
+  if (resetToken == token) {
+    updatePassword(email);
+  }
 });
 
 // Updates password
 const updatePassword = async (email) => {
-    const hash = Hasher.update(req.body.password);
-    User.findOneAndUpdate(
-      query,
-      {
-        name: req.body.name,
-        email: req.body.email,
-        password: hash.digest("hex"),
-      },
-      { upsert: true },
-      (err, doc) => {
-        if (err) return res.status(500).send(err);
-        console.log("User updated");
-        res.send("Succesfully updated.");
-      }
-    );
+  const hash = Hasher.update(req.body.password);
+  User.findOneAndUpdate(
+    query,
+    {
+      name: req.body.name,
+      email: req.body.email,
+      password: hash.digest("hex"),
+    },
+    { upsert: true },
+    (err, doc) => {
+      if (err) return res.status(500).send(err);
+      console.log("User updated");
+      res.send("Succesfully updated.");
+    }
+  );
 };
 
-/// 
-
 const requestPasswordReset = async (email) => {
-    const user = await User.findOne({ email });
-    console.log("print USER")
-    console.log(user);
+  const user = await User.findOne(email);
 
-    //const clientURL = localhost;
+  //const clientURL = localhost;
 
-    /*
-    //if (!user) throw new Error("Email does not exist");
-  
-    let token = await Token.findOne({ userId: user._id });
-    if (token) await token.deleteOne();
-  
-    let resetToken = crypto.randomBytes(32).toString("hex");
-    const hash = await bcrypt.hash(resetToken, Number(bcryptSalt));
-  
-    await new Token({
-      userId: user._id,
-      token: hash,
-      createdAt: Date.now(),
-    }).save();
-  
-    const link = `${clientURL}/passwordReset?token=${resetToken}&id=${user._id}`;
-  
-    sendEmail(
-      email,
-      "Password Reset Request",
-      {
-        name: user.name,
-        link: link,
-      },
-      "./util/forgotPassword.handlebars"
-    );
-    return link;
-    */
-  };
+  //if (!user) throw new Error("Email does not exist");
 
-module.exports = requestPasswordReset;
+  let token = await Token.findOne({ userId: user._id });
+  if (token) await token.deleteOne();
+
+  let resetToken = crypto.randomBytes(32).toString("hex");
+  // const hash = await bcrypt.hash(resetToken, Number(bcryptSalt));
+  const hash = Hasher.update(resetToken);
+
+  await new Token({
+    userId: user._id,
+    token: hash.digest("hex"),
+    createdAt: Date.now(),
+  }).save();
+
+  const link = `http://localhost:8001/passwordReset?token=${resetToken}&id=${user._id}`;
+
+  sendEmail(
+    email,
+    "Password Reset Request",
+    {
+      name: user.name,
+      link: link,
+    },
+    "./util/forgotPassword.handlebars"
+  );
+  return link;
+};
+
+module.exports = app;
--- identity_service/src/util/sendEmail.js
+++ identity_service/src/util/sendEmail.js
@@ -7,42 +7,90 @@ const path = require("path");
 
 const sendEmail = async (email, subject, payload, template) => {
   try {
-
     // Generest test SMTP service from ethereal.email
     let testAccount = await nodemailer.createTestAccount();
 
-    // create reusable transporter object using the default SMTP transport
+    console.log("email generated");
+
+    // // create reusable transporter object using the default SMTP transport
+    // const transporter = nodemailer.createTransport({
+    //   host: "smtp.ethereal.email",
+    //   port: 587,
+    //   secure: false,
+    //   auth: {
+    //     user: testAccount.user,
+    //     pass: testAccount.pass,
+    //   },
+    // });
+
     const transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
-      port: 587,
-      secure: false,
+      service: "gmail",
       auth: {
-        user: testAccount.user,
-        pass: testAccount.pass,
+        type: "OAuth2",
+        user: "leoc.001re@gmail.com",
+        pass: "Panther980928Leo",
+        clientId: process.env.OAUTH_CLIENTID,
+        clientSecret: process.env.OAUTH_CLIENT_SECRET,
+        refreshToken: process.env.OAUTH_REFRESH_TOKEN,
       },
+      debug: true, // show debug output
+      logger: true, // log information in console
     });
 
-    const source = fs.readFileSync(path.join(__dirname, template), "utf8");
-    const compiledTemplate = handlebars.compile(source);
-    const options = () => {
-      return {
-        from: testAccount.user, //UNSURE IF DIS WORKS
-        to: email,
-        subject: subject,
-        html: compiledTemplate(payload),
-      };
-    };
+    // const source = fs.readFileSync(path.join(__dirname, template), "utf8");
+    // const compiledTemplate = handlebars.compile(source);
+    // const options = () => {
+    //   return {
+    //     from: testAccount.user, //UNSURE IF DIS WORKS
+    //     to: email,
+    //     subject: subject,
+    //     html: compiledTemplate(payload),
+    //   };
+    // };
 
-    // Send email
-    transporter.sendMail(options(), (error, info) => {
+    // send mail with defined transport object
+    // let info = await transporter.sendMail({
+    //   from: "mervin.hammes53@ethereal.email",
+    //   to: "leoc.001re@gmail.com",
+    //   subject: "Hello",
+    //   text: "Hello world?",
+    //   html: "<b>Hello world?</b>",
+    // });
+
+    transporter.verify(function (error, success) {
       if (error) {
-        return error;
+        console.log(error);
       } else {
-        return res.status(200).json({
-          success: true,
-        });
+        console.log("Server is ready to take our messages");
       }
     });
+
+    var mailOptions = {
+      from: "leoc.001re@gmail.com",
+      to: "z5261846@ad.unsw.edu.au",
+      subject: "Sending Email using Node.js",
+      text: "That was easy!",
+      html: "<b>Hello world?</b>",
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent: " + info.response);
+      }
+    });
+
+    //   // Send email
+    //   transporter.sendMail(options(), (error, info) => {
+    //     if (error) {
+    //       return error;
+    //     } else {
+    //       return res.status(200).json({
+    //         success: true,
+    //       });
+    //     }
+    //   });
   } catch (error) {
     return error;
   }
