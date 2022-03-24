8567e6aa0bd32b4b12f204b30d2a3317220e0833
Leo Cao <z5261846@ad.unsw.edu.au>
2022-03-25 02:41:52 +1100

    book schema inside user schema

--- identity_service/src/models/userModel.js
+++ identity_service/src/models/userModel.js
@@ -1,10 +1,21 @@
 const mongoose = require("mongoose");
 
+const bookSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  image: { type: String, required: true },
+  authors: { type: String, required: true },
+  rating: { type: String },
+  link: { type: String, required: true },
+  asin: { type: String, required: true },
+  // prices in the future
+});
+
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: false },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    collection: [bookSchema],
   },
   { collection: "Users" }
 );
