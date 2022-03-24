d1e0c482649911c7ebcff4d5f89b3b855585f977
Leo Cao <z5261846@ad.unsw.edu.au>
2022-03-25 02:43:35 +1100

    change port in getting general book collection

--- Front End/src/Pages/LandingPage.js
+++ Front End/src/Pages/LandingPage.js
@@ -35,7 +35,7 @@ export default function LandingPage() {
 
   async function getData() {
     await axios
-      .get("http://localhost:8001/books")
+      .get("http://localhost:8002/books")
       .then((res) => {
         setBooks(res.data);
       })
