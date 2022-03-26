723e6310a66605a9447130fbda7290298e6e59eb
doly <doly@dolys-MacBook-Pro.local>
2022-03-26 22:15:27 +1100

    Added new schemas for collection, review and goal

--- identity_service/src/models/userModel.js
+++ identity_service/src/models/userModel.js
@@ -1,25 +1,65 @@
 const mongoose = require("mongoose");
 
-const bookSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  image: { type: String, required: true },
-  authors: { type: String, required: true },
-  rating: { type: String },
-  link: { type: String, required: true },
-  asin: { type: String, required: true },
-  // prices in the future
-});
-
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: false },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    books: [bookSchema],
+    //books: [bookSchema], // 3. within collections? 
+    goals: [goalSchema],
+    collections: [collectionSchema],
   },
-  { collection: "Users" }
+  { collection: "Users" } // 4. ask
+);
+
+const collectionSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'Users' },
+    name: { type: String, require: true },
+    books: [{ type: mongoose.Schema.Types.ObjectId, ref: 'UserBook' }],
+    status: { type: Boolean, require: true }, // for private/public
+  }
+);
+
+const bookSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    image: { type: String, required: true },
+    authors: { type: String, required: true },
+    rating: { type: String }, // 2. Should this be Number or not matter?
+    link: { type: String, required: true },
+    asin: { type: String, required: true },
+    // prices in the future
+    // added new below
+    category: { type: String, required: true },
+    publisher: { type: String, required: true },
+    date: { type: Date, required: true },
+    reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }], 
+    read: { type: Boolean, required: true },
+  }
+);
+
+const reviewSchema = new mongoose.Schema(
+  {
+  review: { type: String, required: true },
+});
+
+const goalSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'Users' }, // 1. check if this coorect later
+    duration: { type: Number, require: true },
+    target: { type: Number, require: true },
+    current: { type: Number },
+  }
 );
 
 const Users = mongoose.model("Users", userSchema);
+const Collection = mongoose.model("Collection", collectionSchema);
+const UserBook = mongoose.model("UserBook", bookSchema);
+const Review = mongoose.model("Review", reviewSchema);
+const Goal = mongoose.model("Goal", goalSchema);
+
+// QUESITONS
+// 5. is 'books: [bookSchema]' the same thing as 'books: [{ type: mongoose.Schema.Types.ObjectId, ref: 'UserBooks' }]'
 
 module.exports = Users;
