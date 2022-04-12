72cda2745409adc027de0aceeed5c5a7f380e96a
doly <z5207515@student.unsw.edu.au>
2022-04-12 21:28:30 +1000

    removed reviews from userbook

--- identity_service/src/models/userBookModel.js
+++ identity_service/src/models/userBookModel.js
@@ -4,7 +4,6 @@ const bookSchema = new mongoose.Schema(
   {
     bookid: { type: Number, required: true },
     read: { type: Boolean, required: true },
-    reviews: { type: mongoose.Schema.Types.ObjectId, ref: "Reviews"},
     collections: [{ type: mongoose.Schema.Types.ObjectId, ref: "Collections" }],
   },
   { collection: "UserBooks" }
