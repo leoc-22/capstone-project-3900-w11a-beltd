33ab5037999ae76efc0e206d7bd8930351748f48
Leo Cao <z5261846@ad.unsw.edu.au>
2022-04-18 13:27:15 +1000

    rating is number

--- collection_service/src/models/bookModel.js
+++ collection_service/src/models/bookModel.js
@@ -6,12 +6,12 @@ const bookSchema = new mongoose.Schema(
     image: { type: String, required: true },
     authors: { type: String, required: true },
     publisher: { type: String, required: true },
-    rating: { type: String },
+    rating: { type: Number },
     link: { type: String, required: true },
     asin: { type: String, required: true },
     price: { type: String, required: true },
     categories: [{ type: Object, required: true }],
-    
+
     bookid: { type: Number, required: true },
     reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: "Reviews" }],
   },
